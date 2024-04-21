9ceb9701a208e9641116ac29d006fc493f7c7ffc
sameer khan <sameerkhanaa5@gmail.com>
2024-04-21 17:23:39 +0530

    learning event binding and 2 way binding

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
-  name = 'sameer';
+  name = 'sameer khan';
   age = 24;
   salary = 40000;
+  isBtnDisabled = true;
+
+  // onChange(e: any) {
+  //   this.name = e.target.value;
+  //   // console.log(e.target.value);
+  //   // console.log('hello');
+  // }
 }
