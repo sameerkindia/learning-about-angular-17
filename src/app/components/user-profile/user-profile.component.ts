4f3704db9d63120e12dae2c50760addcc77656a5
sameer khan <sameerkhanaa5@gmail.com>
2024-04-22 02:30:42 +0530

    learning Input decorator

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  Input,
+  booleanAttribute,
+  numberAttribute,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+function uppercase(name: String) {
+  return name.toUpperCase();
+}
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
-  name = 'sameer khan';
-  age = 24;
-  salary = 40000;
-  isBtnDisabled = true;
+  @Input({ alias: 'username', transform: uppercase }) name = '';
+  @Input({ transform: numberAttribute }) age!: Number;
+  @Input({ transform: booleanAttribute }) isTrue!: Boolean;
+  // name = 'sameer khan';
+  // age = 24;
+  // salary = 40000;
+  // isBtnDisabled = true;
+
+  // users = [
+  //   { name: 'sameer khan', age: 24 },
+  //   { name: 'sahli', age: 26 },
+  //   { name: 'aamin', age: 22 },
+  // ];
 
   // onChange(e: any) {
   //   this.name = e.target.value;
